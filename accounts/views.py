3bd44a9ce71b2eaec5b0d3e79ce3bdb6b8b87b6a
jyjdev <yun22287@naver.com>
2021-11-17 19:01:19 +0900

    accounts_function

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from django.shortcuts import get_object_or_404, redirect, render
+from rest_framework.response import Response
+from rest_framework import serializers, status
 
-# Create your views here.
+from accounts.serializers import UserSerializer
+
+def signup(request):
+    password = request.data.get('password')
+    password_confirmation = request.data.get('password_confirmation')
+    if password != password_confirmation:
+        return Response({'error':'비밀번호가 일치하지 않습니다'}, status=status.HTTP_400_BAD_REQUEST)
+    
+    serializer = UserSerializer(data=request.data)
+    if serializer.is_valid(raise_exception=True):
+        user = serializer.save()
+        user.set_password(request.data.get('password'))
+        user.save()
+        return Response(serializer.data,status=status.HTTP_201_CREATED)
+
+def follow(request, user_pk):
+    if request.user.is_authenticated:
+        person = get_object_or_404(get_user_model(), pk=user_pk)
+        if request.user != person:
+            if person.followers.filter(pk=request.user_pk).exists():
+                person.followers.remove(request.user)
+            else:
+                person.followers.add(request.user)
+    return Response({'error':'인증되지 않은 사용자입니다'}, status=status.HTTP_401_UNAUTHORIZED)
+
