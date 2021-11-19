028f0142a433320d820e12414676af54c8749c6f
jyjdev <yun22287@naver.com>
2021-11-19 16:11:50 +0900

    modify Community

--- community/serializers.py
+++ community/serializers.py
@@ -5,7 +5,7 @@ class ReviewListSerializers(serializers.ModelSerializer):
     
     class Meta:
         model = Review
-        fields = ('id','title','user','updated_at','movie_id')
+        fields = ('id','title','user','updated_at','movie_id','created_at')
 
 class ReviewSerializers(serializers.ModelSerializer):
     movie = serializers.CharField(read_only=True)
@@ -20,7 +20,7 @@ class ReviewCommentListSerializers(serializers.ModelSerializer):
     
     class Meta:
         model = Review_comment
-        fields = ('id','content','user','updated_at')
+        fields = ('id','content','user','updated_at','created_at')
 
 class ReviewCommentSerializers(serializers.ModelSerializer):
     review = serializers.CharField(read_only=True)
@@ -33,7 +33,7 @@ class ReviewCommentSerializers(serializers.ModelSerializer):
 class ChatboardListSerializers(serializers.ModelSerializer):
     class Meta:
         model = Chatboard
-        fields=('id','title','user','updated_at','board_num')
+        fields=('id','title','user','updated_at','board_num','created_at')
 
 class ChatboardSerializers(serializers.ModelSerializer):
     user = serializers.CharField(read_only=True)
@@ -47,7 +47,7 @@ class ChatboardCommentListSerializers(serializers.ModelSerializer):
     
     class Meta:
         model = Chatboard_comment
-        fields = ('id','content','user','updated_at')
+        fields = ('id','content','user','updated_at','created_at')
 
 class ChatboardCommentSerializers(serializers.ModelSerializer):
     chatboard = serializers.CharField(read_only=True)
