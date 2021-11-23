f41658a374c92427b62f6eb312818bcf76c7bddd
jyjdev <yun22287@naver.com>
2021-11-23 23:29:31 +0900

    modify accounts & community

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,12 +1,14 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
+from django.conf import settings
 
 class UserSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
 
     class Meta:
+        # model = get_user_model()
         model = get_user_model()
-        fields= ('username','password','image',)
+        fields= ('username','password')
 
 class UserListSerializer(serializers.ModelSerializer):
 
--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 from rest_framework_jwt.views import obtain_jwt_token
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('signup/', views.signup),
@@ -8,5 +10,7 @@ urlpatterns = [
     path('userlist/', views.user_list),
     path('api-token-auth/', obtain_jwt_token),
     path('<str:name>/',views.user_detail),
+    path('upload/',views.analyze_image),
     
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
